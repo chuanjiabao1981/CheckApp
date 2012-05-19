4cf2a442765da4b8d195e0d0a1e5eb632d80bc93
guowei <chuanjiabao1981@gmail.com>
2012-05-19 22:40:10 +0800

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   include Api::V1::SessionsHelper
+
+  before_filter :set_locale
+
+	def set_locale
+  	I18n.locale = 'cn'
+	end
 end
